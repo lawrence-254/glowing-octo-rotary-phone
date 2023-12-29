697bf47a6eb4382899ecdef55f8f6779f07fb237
lawrence-254 <lodhiambo26@gmail.com>
2023-12-29 20:57:22 +0300

    generating the access tokken

---
+++ daraja_api/accessToken.php
@@ -0,0 +1,18 @@
+<?php
+// Api Keys
+$consumerKey="DStesc5iXxNE1BzyrDXy5ihm5gwuuQvk";
+$consumerSecret="I83iAhiydWpXdmDu";
+// access token url
+$accessTokenUrl="https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+$headers=['Content-Type:application/json; charset=utf8'];
+$curl=curl_init($accessTokenUrl);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($curl, CURLOPT_HEADER, FALSE);
+curl_setopt($curl, CURLOPT_USERPWD, $consumerKey . ':' . $consumerSecret);
+$result=curl_exec($curl);
+$status=curl_getinfo($curl, CURLINFO_HTTP_CODE);
+echo $result;
+$result=json_decode($result);
+echo $access_token = $result->access_token;
+curl_close($curl);
---
+++ daraja_api/index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo 'gugu';
+ echo 'aaa
+  gaga';
