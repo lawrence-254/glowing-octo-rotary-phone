653bcb769e2958e96753bb0e2b959e9363585948
lawrence-254 <lodhiambo26@gmail.com>
2024-10-02 17:33:12 +0300

    infoForm

--- techwitter/core/urls.py
+++ techwitter/core/urls.py
@@ -6,5 +6,13 @@ urlpatterns = [
     path('home', views.home, name='home'),
     path('register', views.register, name='register'),
     path('login', views.login, name='login'),
-    path('logout', views.logout,  name='logout')
+    path('logout', views.logout,  name='logout'),
+    path('reset', views.reset_password, name='reset'),
+    path('settings', views.settings, name='settings'),
+    path('billing', views.billing, name='billing'),
+    path('notification',views.notification,name='notification'),
+    path('privacy', views.privacy, name='privacy'),
+    path('profile', views.profile, name='profile'),
+    path('security', views.security, name='security'),
+    path('secial_links', views.social_links, name='social_links'),
 ]
--- techwitter/core/views.py
+++ techwitter/core/views.py
@@ -18,7 +18,7 @@ def register(request):
         if password == confirm_password:
             if User.objects.filter(email=email).exists():
                 messages.info(request,'Email is taken, please try another email')
-                return redirect('regster')
+                return redirect('register')
             elif User.objects.filter(username=username).exists():
                 messages.info(request, 'Username is taken, Please try another one')
                 return redirect('register')
@@ -26,11 +26,16 @@ def register(request):
                 user = User.objects.create_user(email=email, username=username, password=password)
                 user.save()
                 # log user in and create a profile object
-
-                user_model =User.objects.get(username=username)
-                new_profile =Profile.objects.create(user=user_model, id_user=user_model.id)
-                new_profile.save()
-                return redirect('login')
+                user_login = auth.authenticate(username=username, password=password)
+                if user_login is not None:
+                    auth.login(request, user_login)
+                    user_model = User.objects.get(username=username)
+                    new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                    new_profile.save()
+                    return redirect('settings')
+                else:
+                    messages.info(request, 'Authentication failed, please try again')
+                    return redirect('register')
         else:
             messages.info(request, 'Passwords has to match')
             return redirect('register')
@@ -76,12 +81,52 @@ def logout(request):
     auth.logout(request)
     return redirect('login')
 
-@login_required(login_url='login')
 def reset_password(request):
-    pass
+    context = {'title': 'RESET'}
+    return render(request, 'core/auth/reset.html', context)
 
 #end of authentication
 
+#profile action
+@login_required(login_url='login')
+def settings(request):
+    context= {'title': 'PROFILE SETTINGS'}
+
+    return render(request, 'core/profile/settings.html', context)
+
+@login_required(login_url='login')
+def billing(request):
+    context={'title': 'BILLING'}
+    return render(request, 'core/profile/billing.html', context)
+
+@login_required(login_url='login')
+def notification(request):
+    context={'title': 'NOTIFICATION'}
+    return render(request, 'core/profile/notification.html', context)
+
+@login_required(login_url='login')
+def privacy(request):
+    context={'title': 'PRIVACY'}
+    return render(request, 'core/profile/privacy.html', context)
+
+@login_required(login_url='login')
+def profile(request):
+    context={'title': 'PROFILE'}
+    return render(request, 'core/profile/profile.html', context)
+
+
+@login_required(login_url='login')
+def security(request):
+    context={'title': 'SECURITY'}
+    return render(request, 'core/profile/security.html', context)
+
+@login_required(login_url='login')
+def social_links(request):
+    context={'title': 'SOCIAL LINKS'}
+    return render(request, 'core/profile/social_links.html', context)
+# end of profile action
+
+# views
 def index(request):
     return render(request, 'core/index.html')
 
@@ -89,3 +134,7 @@ def index(request):
 def home(request):
     context= {'title':'Home'}
     return render(request, 'core/home.html', context)
+
+
+# end of views
+
