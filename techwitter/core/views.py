43c56d5c670732835c171b86d74c286f16da3463
lawrence-254 <lodhiambo26@gmail.com>
2024-10-02 11:50:12 +0300

    title setup

--- techwitter/core/views.py
+++ techwitter/core/views.py
@@ -10,14 +10,13 @@ def home(request):
 
 def register(request):
     context={'title':'REGISTER'}
-
+    print(context)
     if request.method =='POST':
         email = request.POST['email']
         username= request.POST['username']
         password = request.POST['password']
         confirm_password = request.POST['confirmPassword']
         if password == confirm_password:
-            hashed_password = ''
             if User.objects.filter(email=email).exists():
                 messages.info(request,'Email is taken, please try another email')
                 return redirect('regster')
@@ -25,30 +24,40 @@ def register(request):
                 messages.info(request, 'Username is taken, Please try another one')
                 return redirect('register')
             else:
-                user = User.objects.create_user(email=email, username=username, password=hashed_password)
+                user = User.objects.create_user(email=email, username=username, password=password)
                 user.save()
                 # log user in and create a profile object
 
                 user_model =User.objects.get(username=username)
-                new_profile =Profile.objects.create_user(user=user_model, id_user=user_model.id)
+                new_profile =Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('login')
         else:
             messages.info(request, 'Passwords has to match')
             return redirect('register')
-        print(username)
-        print(email)
-        print(password)
-        print(confirm_password)
     else:
         return render(request, 'core/auth/register.html', context)
-    return render(request, 'core/auth/register.html', context)
 
 def login(request):
     context={'title':'LOGIN'}
     if request.method == 'POST':
         email_or_username = request.POST['emailname']
         password = request.POST['password']
-        print(email_or_username)
-        print(password)
+
+        # user_in_database_email = User.objects.get()
+        # print(user_in_database_email)
+
+        if email_or_username == '':
+            messages.info(request, 'Please provide an email, or username')
+            return redirect('login')
+        elif password == '':
+            messages.info(request, 'Please provide a password')
+            return redirect('login')
+        else:
+            pass
+        
     else:
         return render(request, 'core/auth/login.html', context)
-    return render(request, 'core/auth/login.html', context)
+
+def logout(request):
+    pass
