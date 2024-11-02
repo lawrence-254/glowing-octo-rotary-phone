4d3608eabf43594f80fe6ea031a2f1a943904f9d
lawrence-254 <lodhiambo26@gmail.com>
2024-11-02 19:24:52 +0300

    auth

--- techwitter/techwitter/settings.py
+++ techwitter/techwitter/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'core',
     'core.user'
 ]
@@ -111,6 +112,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# api authentication settings
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
--- techwitter/techwitter/urls.py
+++ techwitter/techwitter/urls.py
@@ -21,8 +21,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls'))
+    path('', include('core.urls')),
+    path('api/', include('core.routers'))
 ]
+
 
 urlpatterns = urlpatterns+static(settings.MEDIA_URL,
                                  document_root=settings.MEDIA_ROOT)
