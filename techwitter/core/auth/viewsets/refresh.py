91ea98fe071bc1920ba9f6798e8161b11b49905b
lawrence-254 <lodhiambo26@gmail.com>
2024-11-04 17:29:12 +0300

    auth/refresh

--- techwitter/core/auth/viewsets/refresh.py
+++ techwitter/core/auth/viewsets/refresh.py
@@ -4,3 +4,17 @@ from rest_framework.permissions import AllowAny
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
+
+class RefreshViewSet(viewsets.viewSet, TokenRefreshView):
+    permission_classes=(AllowAny,)
+    http_method_names=['poost']
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise InvalidToken(e.args[0])
+        
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
