869e10c0fbc3da6827b13e49fc24ffe98cf4b8fa
lawrence-254 <lodhiambo26@gmail.com>
2024-11-05 18:50:56 +0300

    abstract

--- techwitter/core/user/models.py
+++ techwitter/core/user/models.py
@@ -11,12 +11,12 @@ from core.abstract.models import AbstractModel, AbstractManager
 User manager model
 """
 class UserManager(BaseUserManager, AbstractManager):
-    def get_object_by_public_id(self, public_id):
-        try:
-            instance = self.get(public_id=public_id)
-            return instance
-        except(ObjectDoesNotExist, ValueError, TypeError):
-            return Http404
+    # def get_object_by_public_id(self, public_id):
+    #     try:
+    #         instance = self.get(public_id=public_id)
+    #         return instance
+    #     except(ObjectDoesNotExist, ValueError, TypeError):
+    #         return Http404
     
     def create_user(self, username, email, password=None, **kwargs):
         """
@@ -57,15 +57,15 @@ class UserManager(BaseUserManager, AbstractManager):
 user model that enables user information to be stored within the database
 """
 class User(AbstractBaseUser, AbstractModel, PermissionsMixin):
-    public_id= models.UUIDField(db_index=True, unique=True, default=uuid.uuid4, editable=False)
+    # public_id= models.UUIDField(db_index=True, unique=True, default=uuid.uuid4, editable=False)
     username = models.CharField(db_index=True, max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(db_index=True, unique=True)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    # created_at = models.DateTimeField(auto_now=True)
+    # updated_at = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
