ba8ea83dde98073d9897acebb417feed544609f0
lawrence-254 <lodhiambo26@gmail.com>
2024-11-07 19:16:13 +0300

    likes

--- techwitter/core/post/viewsets.py
+++ techwitter/core/post/viewsets.py
@@ -12,7 +12,7 @@ class PostViewSet(AbstractViewSet):
     permission_classes = (IsAuthenticated,)
     serializer_class = PostSerializer
 
-    def get_quertset(self):
+    def get_queryset(self):
         return Post.objects.all()
     
     def get_object(self):
@@ -28,4 +28,25 @@ class PostViewSet(AbstractViewSet):
         self.perform_create(serializer)
         return Response(
             serializer.data, status=status.HTTP_201_CREATED
+        )
+    
+    @action(methods=['post'], detail=True)
+    def like(self, request, *args, **kwargs):
+        post = self.get_object()
+        user = self.request.user
+        user.like(post)
+        serializer = self.serializer_class(post)
+
+        return Response(
+            serializer.data, status=status.HTTP_200_OK
+        )
+    
+    @action(methods=['post'], detail=True):
+    def remove_like(self, request, *args, **kwargs):
+        post = self.get_object()
+        user = self.request.user
+        user.remove_like(post)
+        serializer = self.serializer_class(post)
+        return Response(
+            serializer.data, status=status.HTTP_200_OK
         )
