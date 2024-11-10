dbb7266fcafe6b65607ee60e300b13e9ed7de1d9
lawrence-254 <lodhiambo26@gmail.com>
2024-11-10 18:28:36 +0300

    commentsModel

--- techwitter/core/comment/models.py
+++ techwitter/core/comment/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from core.abstract.models import AbstractModel, AbstractManager
 
-# Create your models here.
+class CommentManager(AbstractManager):
+    pass
+
+class Comment(AbstractModel):
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    author = 
