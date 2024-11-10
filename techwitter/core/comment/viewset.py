610ef9a278570278a3058bed71fe5522944c9d7f
lawrence-254 <lodhiambo26@gmail.com>
2024-11-10 22:01:47 +0300

    nestedRoutes

--- techwitter/core/comment/viewset.py
+++ techwitter/core/comment/viewset.py
@@ -12,3 +12,13 @@ class CommentViewSet(AbstractViewSet):
     http_method_names = ('post', 'get', 'put', 'delete')
     permision_classes=(UserPermission,)
     serializer_class=CommentSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Comment.objects.all()
+        
+        post_pk = self.kwargs['post_pk']
+        if post_pk is None:
+            return Http404
+        queryset =Comment.objects.filter(post__public_id=post_pk)
+        return queryset
