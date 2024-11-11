85975864cedf8fa98605522190fc416b7db40505
lawrence-254 <lodhiambo26@gmail.com>
2024-11-11 22:20:32 +0300

    first test case

--- techwitter/core/user/tests.py
+++ techwitter/core/user/tests.py
@@ -9,3 +9,15 @@ data_user ={
     'first_name': 'test',
     'last_name': 'user'
 }
+
+@pytest.mark.django_db
+def test_create_user():
+    user = User.objects.create_user(**data_user)
+    assert user.username == data_user['username']
+    assert user.email == data_user['email']
+    assert user.first_name == data_user['first_name']
+    assert user.last_name == data_user['last_name']
+    assert user.is_active == True
+    assert user.is_staff == False
+    assert user.is_superuser == False
+    assert user.check_password(data_user['password']) == True
