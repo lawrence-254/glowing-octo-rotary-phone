da7d4d62ede09d11d774eea4e600df0acfbd8d20
lawrence-254 <lodhiambo26@gmail.com>
2024-11-11 16:31:56 +0300

    serializerXtra

--- techwitter/core/comment/viewset.py
+++ techwitter/core/comment/viewset.py
@@ -22,3 +22,16 @@ class CommentViewSet(AbstractViewSet):
             return Http404
         queryset =Comment.objects.filter(post__public_id=post_pk)
         return queryset
+    
+    def get_object(self):
+        obj = Comment.objects.get_object_by_public_id(self.kwargs['pk'])
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+    def create(self,request,*args,**kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED
+        )
