2cdcad3fed035930057d36ab9fc5f0e8f0eb5faf
lawrence-254 <lodhiambo26@gmail.com>
2024-11-12 11:02:46 +0300

    loginTest

--- techwitter/core/auth/tests.py
+++ techwitter/core/auth/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from rest_framework import status
+
+from core.fixtures.user import user
+
+from core.user.models import User
+
+class TestAuthenticationViewSet:
+    endpoint = '/api/auth/'
+
+    def test_login(self, client, user):
+        data={
+            "email": user.email,
+            "password" : "testpassword"
+        }
+
+        response = client.post(self.endpoint + "login/", data)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['access']
+        assert response.data['user']['id'] == user.public_id.hex
+        assert response.data['user']['username'] == user.username
+        assert response.data['user']['email'] == user.email
+    
