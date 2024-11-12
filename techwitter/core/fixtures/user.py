5b2db43fa19ca45e65b9aaa32b0f2b477dd420a3
lawrence-254 <lodhiambo26@gmail.com>
2024-11-12 10:02:57 +0300

    postFixtureText

--- techwitter/core/fixtures/user.py
+++ techwitter/core/fixtures/user.py
@@ -2,12 +2,19 @@ import pytest
 
 from core.user.models import User
 
-data_user={
-    "username": "user",
-    "email": "test@testing.mail",
-    "firstname": "test",
-    "lastname": "user",
-    "password": "12_not@realpassword"
+# data_user={
+#     "username": "user",
+#     "email": "test@testing.mail",
+#     "firstname": "test",
+#     "lastname": "user",
+#     "password": "12_not@realpassword"
+# }
+data_user ={
+    'username': 'testuser',
+    'email': 'test@mail.com',
+    'password': 'testpassword',
+    'first_name': 'test',
+    'last_name': 'user'
 }
 
 @pytest.fixture
