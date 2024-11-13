e0ce6a7cf84cba20b779001b4e0555b954c0f1f6
lawrence-254 <lodhiambo26@gmail.com>
2024-11-13 21:56:11 +0300

    0

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components"
+
+import React from 'react'
+
+const HomeContainerMain = styled.div`
+background: red
+`
+const HomeTitle = styled.h1`
+color: pink;
+display: flex;
+alignSelf: center
+`
+
+const Home = () => {
+  return (
+    <HomeContainerMain>
+        <HomeTitle>
+            prof
+        </HomeTitle>
+        Home
+    </HomeContainerMain>
+  )
+}
+
+export default Home
