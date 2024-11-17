be7a064c4f2bc258de1368fc67a37e5e2dd7b58a
lawrence-254 <lodhiambo26@gmail.com>
2024-11-17 21:22:08 +0300

    plaincsslogin

--- client/src/pages/authentication/Login.jsx
+++ client/src/pages/authentication/Login.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import LoginForm from "../../components/forms/LoginForm"; 
-import { Container, Row, Col, Button, Card } from "react-bootstrap";
-
+import '../../css/pages/authentication/Login.css'
 const Login = () => {
   return (
-    <Container fluid className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
-      <Row className="w-100">
+    <div className="login-container">
+      <div className="login-row">
         {/* Left Column: Title and Intro */}
-        <Col xs={12} md={6} className="d-flex w-50 justify-content-center flex-column align-items-start p-4">
-          <h1 className="mb-4">TechWitter</h1>
-          <p className="text-muted mb-3">Welcome to your tech discussion forum</p>
-          <hr></hr>
-          <p className="text-muted mb-4">Don't have an account?</p>
-          <Link to="/register/">
-            <Button variant="outline-primary" size="lg">REGISTER</Button>
+        <div className="left-column">
+          <h1>TechWitter</h1>
+          <p>Welcome to your tech discussion forum</p>
+          <hr />
+          <p>Don't have an account?</p>
+          <Link to="/register/" className="button-link">
+            <button className="btn btn-outline-primary btn-lg">REGISTER</button>
           </Link>
-        </Col>
+        </div>
 
         {/* Right Column: Login Form */}
-        <Col xs={12} md={6} className="d-flex justify-content-center align-items-end p-4">
-          <Card className="w-100 p-4">
-            <Card.Body>
+        <div className="right-column">
+          <div className="card">
+            <div className="card-body">
               <LoginForm />
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default Login;
+
