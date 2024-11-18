5a62f1d1db4e327d177a18de7f5c414f1a894738
lawrence-254 <lodhiambo26@gmail.com>
2024-11-18 21:54:34 +0300

    navbar

--- client/src/components/navigation/Layout.jsx
+++ client/src/components/navigation/Layout.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Navigationbar from "./Navbar";
-import "./Layout.css";
 import "../../css/navigation/Layout.css";
 
 function Layout(props) {
--- client/src/components/navigation/Navbar.jsx
+++ client/src/components/navigation/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./Navigationbar.css"; 
 import "../../css/navigation/Navbar.css";
 
 function Navigationbar() {
