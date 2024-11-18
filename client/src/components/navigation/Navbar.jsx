ddb5e54395b5cb8705b9d802d67e82f2a6e99d2c
lawrence-254 <lodhiambo26@gmail.com>
2024-11-18 21:51:04 +0300

    navbar

--- client/src/components/navigation/Navbar.jsx
+++ client/src/components/navigation/Navbar.jsx
@@ -1,32 +1,35 @@
-import React from 'react';
-import {Navbar, Container, Image, NavDropdown, Nav} from "react-bootstrap";
-import {useNavigate} from "react-router-dom";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Navigationbar.css"; 
+import "../../css/navigation/Navbar.css";
 
-function Navigationbar(){
-    const navigate = useNavigate()
-    const handleLogout=()=>{
-        localStorage.removeItem("auth");
-        navigate('/login/')
-    }
-    return(
-        <Navbar bg="primary" variant="dark">
-            <Container>
-                <Navbar.Brand className="fw-bold" href="#home">TechWitter</Navbar.Brand>
-                <Navbar.Collapse className="justify-content-end">
-                    <Nav>
-                        <NavDropdown
-                        title={
-                            <Image src="#" roundedCircle width={36} height={36}/>
-                        }>
-                            <NavDropdown.Item href='#'>Profile</NavDropdown.Item>
-                            <NavDropdown.Item onClick={handleLogout}>LOGOUT</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
+function Navigationbar() {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("auth");
+    navigate("/login/");
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <a href="#home" className="navbar-brand">TechWitter</a>
+        <div className="navbar-profile">
+          <img
+            src="#"
+            alt="User Avatar"
+            className="navbar-avatar"
+          />
+          <ul className="dropdown">
+            <li><a href="#">Profile</a></li>
+            <li><button onClick={handleLogout}>Logout</button></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navigationbar;
+
 
