b760199cd4a4414b1a6b4b77768faad1a28678c2
lawrence-254 <lodhiambo26@gmail.com>
2024-11-19 15:15:12 +0300

    setupcss

---
+++ client/src/components/post/Post.jsx
@@ -0,0 +1,38 @@
+import React,{useState} from 'react';
+import {format}from'timeago.js';
+import { LikeFilled, CommentOutlined, LikeOutlined} from"@ant-design/icons";
+import {Image, Card, Dropdown} from "react-bootstrap";
+import axiosService from "../../helpers/axios";
+import "../../css/components/post/Post.css";
+
+
+function Post(props){
+    const {post,refresh} = props;
+    const handleClick = ()=>{
+        axiosService.post(`/post/${post.id}/${action}/`).then(()=>{
+            refresh();
+        }).catch((err)=>console.log(err))
+    };
+    return(
+        <>
+        <Card className='card'>
+            <div className="body">
+                <h3 className="title">
+                    <div className='image-container'>
+                        <Image
+                        src=""
+                        roundedCircle
+                        width={48}
+                        height={48}
+                        className='image'
+                        />
+                    </div>
+                </h3>
+            </div>
+
+        </Card>
+        </>
+    )
+}
+
+export default Post
